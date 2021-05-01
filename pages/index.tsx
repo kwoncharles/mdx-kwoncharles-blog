6c42a54d79d8cc76a3f81468164921b9733388f8
kwoncharles <hello.kwon.charles@gmail.com>
2021-05-02 00:00:45 +0900

    chore: change directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import HomePage from '@/screen/home';
 import Layout from '@/components/layout';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -10,7 +10,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <HomePage />
+        <Link
+          href="/post/improve-your-hooks-with-ref"
+          passHref
+        >
+          <a>
+            go to post
+          </a>
+        </Link>
       </Layout>
     </>
   );
